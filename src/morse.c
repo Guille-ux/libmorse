eb4e8cd72a4b0d17d38aace12a62c2dd49db24e6
Guillermo leira temes <104579306+Guille-ux@users.noreply.github.com>
2025-06-04 15:58:02 +0100

    Update morse.c
    making it better

--- src/morse.c
+++ src/morse.c
@@ -79,42 +79,42 @@ Character ascii2character(char asciiCode) {
 }
 
 void character2morse(Character chara, char *buffer) {
-  case MorseA: libmorse_memcpy(".-/", buffer, 3); break; // barras laterales separan simbolo
-  case MorseB: libmorse_memcpy("-.../", buffer, 5); break;
-  case MorseC: libmorse_memcpy("-.-./", buffer, 5); break;
-  case MorseD: libmorse_memcpy("-../", buffer, 4); break;
-  case MorseE: libmorse_memcpy("./", buffer, 2); break;
-  case MorseF: libmorse_memcpy("..-./", buffer, 5); break;
-  case MorseG: libmorse_memcpy("--./", buffer, 4); break;
-  case MorseH: libmorse_memcpy("..../", buffer, 5); break;
-  case MorseI: libmorse_memcpy("../", buffer, 3); break;
-  case MorseJ: libmorse_memcpy(".---/", buffer, 5); break;
-  case MorseK: libmorse_memcpy("-.-/", buffer, 4); break;
-  case MorseL: libmorse_memcpy(".-../", buffer, 5); break;
-  case MorseM: libmorse_memcpy("--/", buffer, 3); break;
-  case MorseN: libmorse_memcpy("-./", buffer, 3); break;
-  case MorseO: libmorse_memcpy("---/", buffer, 4); break;
-  case MorseP: libmorse_memcpy(".--./", buffer, 5); break;
-  case MorseQ: libmorse_memcpy("--.-/", buffer, 5); break;
-  case MorseR: libmorse_memcpy(".-./", buffer, 4); break;
-  case MorseS: libmorse_memcpy(".../", buffer, 4; break;
-  case MorseT: libmorse_memcpy("-/", buffer, 2; break;
-  case MorseU: libmorse_memcpy("..-/", buffer, 4); break;
-  case MorseV: libmorse_memcpy("...-/", buffer, 5); break;
-  case MorseW: libmorse_memcpy(".--/", buffer, 4); break;
-  case MorseX: libmorse_memcpy("-..-/", buffer, 5); break;
-  case MorseY: libmorse_memcpy("-.-/", buffer, 4); break;
-  case MorseZ: libmorse_memcpy("--../", buffer, 5); break;
-  case Morse0: libmorse_memcpy("-----/", buffer, 6); break;
-  case Morse1: libmorse_memcpy(".----/", buffer, 6); break;
-  case Morse2: libmorse_memcpy("..---/", buffer, 6); break;
-  case Morse3: libmorse_memcpy("...--/", buffer, 6); break;
-  case Morse4: libmorse_memcpy("....-/", buffer, 6); break;
-  case Morse5: libmorse_memcpy("...../", buffer, 6); break;
-  case Morse6: libmorse_memcpy("-..../", buffer, 6); break;
-  case Morse7: libmorse_memcpy("--.../", buffer, 6); break;
-  case Morse8: libmorse_memcpy("---../", buffer, 6); break;
-  case Morse9: libmorse_memcpy("----./", buffer, 6); break;
+  case MorseA: libmorse_memcpy((uint8_t *)".-", (uint8_t *)buffer, 2); break; // barras laterales separan simbolo
+  case MorseB: libmorse_memcpy((uint8_t *)"-...", (uint8_t *)buffer, 4); break;
+  case MorseC: libmorse_memcpy((uint8_t *)"-.-.", (uint8_t *)buffer, 4); break;
+  case MorseD: libmorse_memcpy((uint8_t *)"-..", (uint8_t *)buffer, 3); break;
+  case MorseE: libmorse_memcpy((uint8_t *)".", (uint8_t *)buffer, 1); break;
+  case MorseF: libmorse_memcpy((uint8_t *)"..-.", (uint8_t *)buffer, 4); break;
+  case MorseG: libmorse_memcpy((uint8_t *)"--.", (uint8_t *)buffer, 3); break;
+  case MorseH: libmorse_memcpy((uint8_t *)"....", (uint8_t *)buffer, 4); break;
+  case MorseI: libmorse_memcpy((uint8_t *)"..", (uint8_t *)buffer, 2); break;
+  case MorseJ: libmorse_memcpy((uint8_t *)".---", (uint8_t *)buffer, 4); break;
+  case MorseK: libmorse_memcpy((uint8_t *)"-.-", (uint8_t *)buffer, 3); break;
+  case MorseL: libmorse_memcpy((uint8_t *)".-..", (uint8_t *)buffer, 4); break;
+  case MorseM: libmorse_memcpy((uint8_t *)"--", (uint8_t *)buffer, 2); break;
+  case MorseN: libmorse_memcpy((uint8_t *)"-.", (uint8_t *)buffer, 2); break;
+  case MorseO: libmorse_memcpy((uint8_t *)"---", (uint8_t *)buffer, 3); break;
+  case MorseP: libmorse_memcpy((uint8_t *)".--.", (uint8_t *)buffer, 4); break;
+  case MorseQ: libmorse_memcpy((uint8_t *)"--.-", (uint8_t *)buffer, 4); break;
+  case MorseR: libmorse_memcpy((uint8_t *)".-.", (uint8_t *)buffer, 3); break;
+  case MorseS: libmorse_memcpy((uint8_t *)"...", (uint8_t *)buffer, 3; break;
+  case MorseT: libmorse_memcpy((uint8_t *)"-", (uint8_t *)buffer, 1; break;
+  case MorseU: libmorse_memcpy((uint8_t *)"..-", (uint8_t *)buffer, 3); break;
+  case MorseV: libmorse_memcpy((uint8_t *)"...-", (uint8_t *)buffer, 4); break;
+  case MorseW: libmorse_memcpy((uint8_t *)".--", (uint8_t *)buffer, 3); break;
+  case MorseX: libmorse_memcpy((uint8_t *)"-..-", (uint8_t *)buffer, 4); break;
+  case MorseY: libmorse_memcpy((uint8_t *)"-.-", (uint8_t *)buffer, 3); break;
+  case MorseZ: libmorse_memcpy((uint8_t *)"--..", (uint8_t *)buffer, 4); break;
+  case Morse0: libmorse_memcpy((uint8_t *)"-----", (uint8_t *)buffer, 5); break;
+  case Morse1: libmorse_memcpy((uint8_t *)".----", (uint8_t *)buffer, 5); break;
+  case Morse2: libmorse_memcpy((uint8_t *)"..---", (uint8_t *)buffer, 5); break;
+  case Morse3: libmorse_memcpy((uint8_t *)"...--", (uint8_t *)buffer, 5); break;
+  case Morse4: libmorse_memcpy((uint8_t *)"....-", (uint8_t *)buffer, 5); break;
+  case Morse5: libmorse_memcpy((uint8_t *)".....", (uint8_t *)buffer, 5); break;
+  case Morse6: libmorse_memcpy((uint8_t *)"-....", (uint8_t *)buffer, 5); break;
+  case Morse7: libmorse_memcpy((uint8_t *)"--...", (uint8_t *)buffer, 5); break;
+  case Morse8: libmorse_memcpy((uint8_t *)"---..", (uint8_t *)buffer, 5); break;
+  case Morse9: libmorse_memcpy((uint8_t *)"----.", (uint8_t *)buffer, 5); break;
   default: return;
   }
 }
